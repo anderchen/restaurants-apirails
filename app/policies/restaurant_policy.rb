f284d47565eb20befbb5e56bb3869cb5f2e806a4
Anderson Chen <ander.chen00@gmail.com>
2019-01-31 23:45:24 -0200

    API restaurant#destroy working

--- app/policies/restaurant_policy.rb
+++ app/policies/restaurant_policy.rb
@@ -13,4 +13,13 @@ class RestaurantPolicy < ApplicationPolicy
     # Only restaurant owner can update
     record.user == user
   end
+
+  def create?
+    # Any logged in can create a restaurant
+    !user.nil?
+  end
+
+  def destroy?
+    update?
+  end
 end
